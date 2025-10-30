a7dcecdf6c97a5b93030058f1a9a07014d68b488
Abdunazar <abdunazaribragimov945@gmail.com>
2025-10-30 17:10:14 +0500

    done matrix 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,5 +7,5 @@ import { ConfigModule } from '@nestjs/config';
   imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
-})
+}) 
 export class AppModule {}
